c779fbb3920440fc832ca560e42f10ddefa95660
Admin <Admin@THARUN-DESKTOP>
2024-03-07 20:37:01 -0600

    Add project files.

---
+++ MaterialsSeachResult.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataModels
+{
+    public class MaterialsSearchResult
+    {
+        [Required(ErrorMessage = "MaterialId is required")]
+        public int MaterialId { get; set; }
+        public string MaterialName { get; set; }
+        public string MaterialCode { get; set; }
+        public string SourceSystem { get; set; }
+
+    }
+}
